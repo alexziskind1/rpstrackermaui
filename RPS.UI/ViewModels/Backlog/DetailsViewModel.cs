08eec74f86f82287fa7238b14ea232a583af5738
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-05-10 13:40:27 -0400

    [QoL] Use file-scope namespaces in ViewModel folder

--- RPS.UI/ViewModels/Backlog/DetailsViewModel.cs
+++ RPS.UI/ViewModels/Backlog/DetailsViewModel.cs
@@ -6,101 +6,100 @@ using System.Collections.ObjectModel;
 using RPS.UI.BL;
 
 
-namespace RPS.UI.ViewModels.Backlog
+namespace RPS.UI.ViewModels.Backlog;
+
+public partial class DetailsViewModel : ObservableObject
 {
-    public partial class DetailsViewModel : ObservableObject
+    private readonly IPtItemsRepository itemsRepo;
+    private readonly IPtTasksRepository tasksRepo;
+
+    public PtItem TheItem { get; set; }
+
+    [ObservableProperty]
+    public string title;
+
+    [ObservableProperty]
+    public object selection = "Details";
+
+    [ObservableProperty]
+    private bool tab1Visible;
+
+    [ObservableProperty]
+    private bool tab2Visible;
+
+    partial void OnSelectionChanged(object value)
     {
-        private readonly IPtItemsRepository itemsRepo;
-        private readonly IPtTasksRepository tasksRepo;
-
-        public PtItem TheItem { get; set; }
-
-        [ObservableProperty]
-        public string title;
-
-        [ObservableProperty]
-        public object selection = "Details";
-
-        [ObservableProperty]
-        private bool tab1Visible;
-
-        [ObservableProperty]
-        private bool tab2Visible;
-
-        partial void OnSelectionChanged(object value)
+        var valStr = value as string;
+        if (valStr == "Details")
         {
-            var valStr = value as string;
-            if (valStr == "Details")
-            {
-                Tab1Visible = true;
-                Tab2Visible = false;
-            }
-            else if (valStr == "Tasks")
-            {
-                Tab1Visible = false;
-                Tab2Visible = true;
-            }
+            Tab1Visible = true;
+            Tab2Visible = false;
         }
-
-        public DetailsScreenViewModel DetailsScreenVm { get; set; }
-        public TasksScreenViewModel TasksScreenVm { get; set; }
-
-        public DetailsViewModel(PtItem item, IPtItemsRepository iPtRepo, IPtTasksRepository tPtRepo)
+        else if (valStr == "Tasks")
         {
-            this.itemsRepo = iPtRepo;
-            this.tasksRepo = tPtRepo;
-
-            TheItem = item;
-            title = item.Title;
-            Tab1Visible = selection.ToString().Equals("Details");
-            tab2Visible = selection.ToString().Equals("Tasks");
-
-            DetailsScreenVm = new DetailsScreenViewModel(item);
-            TasksScreenVm = new TasksScreenViewModel(item);
-
-            TasksScreenVm.DeleteTaskEvent += new DeleteTaskEventHandler(DeleteTask);
-            TasksScreenVm.SaveNewTaskEvent += new SaveNewTaskEventHandler(SaveNewTask);
+            Tab1Visible = false;
+            Tab2Visible = true;
         }
+    }
 
-        public void DeleteTask(object sender, DeleteTaskEventArgs e)
+    public DetailsScreenViewModel DetailsScreenVm { get; set; }
+    public TasksScreenViewModel TasksScreenVm { get; set; }
+
+    public DetailsViewModel(PtItem item, IPtItemsRepository iPtRepo, IPtTasksRepository tPtRepo)
+    {
+        this.itemsRepo = iPtRepo;
+        this.tasksRepo = tPtRepo;
+
+        TheItem = item;
+        title = item.Title;
+        Tab1Visible = selection.ToString().Equals("Details");
+        tab2Visible = selection.ToString().Equals("Tasks");
+
+        DetailsScreenVm = new DetailsScreenViewModel(item);
+        TasksScreenVm = new TasksScreenViewModel(item);
+
+        TasksScreenVm.DeleteTaskEvent += new DeleteTaskEventHandler(DeleteTask);
+        TasksScreenVm.SaveNewTaskEvent += new SaveNewTaskEventHandler(SaveNewTask);
+    }
+
+    public void DeleteTask(object sender, DeleteTaskEventArgs e)
+    {
+        var result = tasksRepo.DeleteTask(e.Task.Id, TheItem.Id);
+        if (result)
         {
-            var result = tasksRepo.DeleteTask(e.Task.Id, TheItem.Id);
-            if (result)
-            {
-                var currentTasks = tasksRepo.GetAllForItem(TheItem.Id);
-                TheItem.Tasks = currentTasks.ToList();
-                TasksScreenVm.Tasks = new ObservableCollection<PtTask>(TheItem.Tasks);
-            }
+            var currentTasks = tasksRepo.GetAllForItem(TheItem.Id);
+            TheItem.Tasks = currentTasks.ToList();
+            TasksScreenVm.Tasks = new ObservableCollection<PtTask>(TheItem.Tasks);
         }
+    }
 
-        public void SaveNewTask(object sender, SaveNewTaskEventArgs e)
+    public void SaveNewTask(object sender, SaveNewTaskEventArgs e)
+    {
+        var newTask = tasksRepo.AddNewTask(e.NewTask);
+        if (newTask != null)
         {
-            var newTask = tasksRepo.AddNewTask(e.NewTask);
-            if (newTask != null)
-            {
-                var currentTasks = tasksRepo.GetAllForItem(TheItem.Id);
-                TheItem.Tasks = currentTasks.ToList();
-                TasksScreenVm.Tasks = new ObservableCollection<PtTask>(TheItem.Tasks);
-            }
+            var currentTasks = tasksRepo.GetAllForItem(TheItem.Id);
+            TheItem.Tasks = currentTasks.ToList();
+            TasksScreenVm.Tasks = new ObservableCollection<PtTask>(TheItem.Tasks);
         }
+    }
 
-        public void SaveItem()
+    public void SaveItem()
+    {
+        Title = DetailsScreenVm.Title;
+
+        var updateItem = new PtUpdateItem
         {
-            Title = DetailsScreenVm.Title;
+            Id = TheItem.Id,
+            Title = DetailsScreenVm.Title,
+            Description = DetailsScreenVm.Description,
+            Type = DetailsScreenVm.SelectedItemTypeEnum,
+            AssigneeId = TheItem.Assignee.Id,
+            Estimate = Convert.ToInt32(DetailsScreenVm.Estimate),
+            Priority = DetailsScreenVm.SelectedPriorityEnum,    
+            Status = DetailsScreenVm.SelectedStatusEnum   
+        };
 
-            var updateItem = new PtUpdateItem
-            {
-                Id = TheItem.Id,
-                Title = DetailsScreenVm.Title,
-                Description = DetailsScreenVm.Description,
-                Type = DetailsScreenVm.SelectedItemTypeEnum,
-                AssigneeId = TheItem.Assignee.Id,
-                Estimate = Convert.ToInt32(DetailsScreenVm.Estimate),
-                Priority = DetailsScreenVm.SelectedPriorityEnum,    
-                Status = DetailsScreenVm.SelectedStatusEnum   
-            };
-
-            var updatedItem = itemsRepo.UpdateItem(updateItem);
-        }
+        var updatedItem = itemsRepo.UpdateItem(updateItem);
     }
 }
--- RPS.UI/ViewModels/Backlog/TasksScreenViewModel.cs
+++ RPS.UI/ViewModels/Backlog/TasksScreenViewModel.cs
@@ -5,51 +5,50 @@ using RPS.UI.Events;
 using System.Collections.ObjectModel;
 
 
-namespace RPS.UI.ViewModels.Backlog
+namespace RPS.UI.ViewModels.Backlog;
+
+public partial class TasksScreenViewModel : ObservableObject
 {
-    public partial class TasksScreenViewModel : ObservableObject
+
+    [ObservableProperty]
+    public string taskName;
+
+    [ObservableProperty]
+    public ObservableCollection<PtTask> tasks;
+
+    [ObservableProperty]
+    public PtTask selectedTask;
+
+    private PtItem TheItem { get; set; }
+
+    public event DeleteTaskEventHandler DeleteTaskEvent;
+    public event SaveNewTaskEventHandler SaveNewTaskEvent;
+
+    public TasksScreenViewModel(PtItem item) 
     {
-
-        [ObservableProperty]
-        public string taskName;
-
-        [ObservableProperty]
-        public ObservableCollection<PtTask> tasks;
-
-        [ObservableProperty]
-        public PtTask selectedTask;
-
-        private PtItem TheItem { get; set; }
-
-        public event DeleteTaskEventHandler DeleteTaskEvent;
-        public event SaveNewTaskEventHandler SaveNewTaskEvent;
-
-        public TasksScreenViewModel(PtItem item) 
-        {
-            TheItem = item;
-            Tasks = new ObservableCollection<PtTask>(item.Tasks.Where(t=>t.DateDeleted == null));
-        }
-
-        public void DeleteSelectedTask()
-        {
-            DeleteTaskEventArgs args = new DeleteTaskEventArgs(SelectedTask);
-            DeleteTaskEvent(this, args);
-        }
-
-
-        public void SaveNewTask()
-        {
-            PtNewTask newTask = new PtNewTask
-            {
-                ItemId = TheItem.Id,
-                Title = TaskName
-            };
-
-            SaveNewTaskEventArgs args = new SaveNewTaskEventArgs(newTask);
-            SaveNewTaskEvent(this, args);
-
-            TaskName = string.Empty;
-        }
-
+        TheItem = item;
+        Tasks = new ObservableCollection<PtTask>(item.Tasks.Where(t=>t.DateDeleted == null));
     }
+
+    public void DeleteSelectedTask()
+    {
+        DeleteTaskEventArgs args = new DeleteTaskEventArgs(SelectedTask);
+        DeleteTaskEvent(this, args);
+    }
+
+
+    public void SaveNewTask()
+    {
+        PtNewTask newTask = new PtNewTask
+        {
+            ItemId = TheItem.Id,
+            Title = TaskName
+        };
+
+        SaveNewTaskEventArgs args = new SaveNewTaskEventArgs(newTask);
+        SaveNewTaskEvent(this, args);
+
+        TaskName = string.Empty;
+    }
+
 }
